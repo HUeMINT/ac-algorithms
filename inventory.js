ef7aca4f40de93545d3b7bd2b0b5678d1b9a3eb2
HUe MINT <HUeMINT@anniecannons.com>
2017-12-19 15:27:08 -0800

    Completed inventory.js

--- inventory.js
+++ inventory.js
@@ -3,7 +3,14 @@
 //Compare and update the inventory stored in an array against a second array of a fresh delivery. Update the current existing inventory item quantities (in arr1). If an item cannot be found, add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item. 
 
 function updateInventory(arr1, arr2) {
-    // All inventory must be accounted for or you're fired!
+    for (var i = 0; i < arr1.length; i++){
+        for (var j = 0; j < arr2.length; j++){
+            if (arr2[i][1] === arr1[j][1]){
+                arr1[j][0] += arr2[i][0];
+            }
+        }
+    }
+    // console.log(arr1);
 }
 
 // Example inventory lists
@@ -22,4 +29,3 @@ var newInv = [
 ];
 
 updateInventory(currentInv, newInv);
-
